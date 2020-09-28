7514a0f7fa62b2e9a855b606ab27d49326e89c87
Adam Mihajlovic <mihajlovicadam@gmail.com>
2020-09-27 20:05:25 -0400

    Change 2

--- Lab2WS/FileReader.cs
+++ Lab2WS/FileReader.cs
@@ -5,7 +5,7 @@
         public string[] Read(string filename)
         {
             // Implement this using info from the slides.
-
+            string[] lines = FileReader.ReadAllLines(filename);
 
             return null;
         }
